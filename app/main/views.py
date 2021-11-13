7f42e018b9279d87fc469dbdac67d676da4b9c10
mugambi645 <mugambi645@gmail.com>
2021-11-13 16:27:10 +0300

    added main page logic

---
+++ app/main/views.py
@@ -0,0 +1,8 @@
+from flask import render_template,request
+from . import main
+@main.route('/')
+def index():
+    '''
+    view root page function that returns the index page and its data
+    '''
+    return render_template('index.html')
